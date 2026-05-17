ada22b8cbeaf981784fb81eb8b0e28091ad44002
hmndzzl <men241265@uvg.edu.gt>
2026-05-17 01:24:19 -0600

    feat: agregar manejo de sesiones expiradas

--- app/frontend/src/components/ui/ProtectedRoute.tsx
+++ app/frontend/src/components/ui/ProtectedRoute.tsx
@@ -1,8 +1,8 @@
 import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../../context/AuthContext';
 
 export const ProtectedRoute = () => {
-    // Verificar si existe el token en el almacenamiento local
-    const token = localStorage.getItem('token');
+    const { token } = useAuth();
 
     // Si no hay token, lo expulsa de vuelta a la pantalla de login
     if (!token) {
